35aee22f4973c653b4d123186939b34167484f0f
phuoc7701 <101518171+phuoc7701@users.noreply.github.com>
2025-04-25 10:25:35 +0700

    Initial commit

---
+++ src/main/java/vn/edu/English/EnglishApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.English;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/English/EnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.English;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
