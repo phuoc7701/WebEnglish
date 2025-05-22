161e9bb3bb013e899c9ce37607ab88bdd25ad2e7
hmquandec05 <quan.m2003@gmail.com>
2025-05-22 23:51:10 +0700

    [backend] feat(upload-file): upload files to the Cloudinary

--- src/main/java/vn/edu/engzone/EngZoneApplication.java
+++ src/main/java/vn/edu/engzone/EngZoneApplication.java
@@ -2,13 +2,8 @@ package vn.edu.engzone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@EnableJpaRepositories(basePackages = "vn.edu.engzone.repository")
-@EntityScan(basePackages = "vn.edu.engzone.entity")
 public class EngZoneApplication {
 
 	public static void main(String[] args) {
